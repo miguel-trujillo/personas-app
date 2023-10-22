edcdc5747885b2713afec53f1ceeb12aff9c8d8d
Miguel Trujillo <trujillotamayo.miguel@gmail.com>
2023-10-22 15:35:26 -0500

    Ajuste de la vista del listado de comunas

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -2,7 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comuna;
+use App\Models\Pais;
+use App\Models\Departamento;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ComunaController extends Controller
 {
@@ -13,13 +17,13 @@ class ComunaController extends Controller
      */
     public function index()
     {
-         //$comunas = Comuna::all();
-         $comunas = DB::table('tb_comuna')
-         ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
-         ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
-         ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
-         ->get();
-     return view("comunas.index", ["comunas" => $comunas]);
+        //$comunas = Comuna::all();
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
+        return view("comunas.index", ["comunas" => $comunas]);
     }
 
     /**
@@ -29,7 +33,15 @@ class ComunaController extends Controller
      */
     public function create()
     {
-        //
+        $municipios = DB::table('tb_municipio')->orderBy('muni_nomb')->get();
+        $departamentos = DB::table('tb_departamento')->orderBy('depa_nomb')->get();
+        $paises = DB::table('tb_pais')->orderBy('pais_nomb')->get();
+    
+        return view('comunas.new', [
+            'municipios' => $municipios,
+            'departamentos' => $departamentos,
+            'paises' => $paises,
+        ]);
     }
 
     /**
@@ -40,8 +52,24 @@ class ComunaController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $comuna = new Comuna();
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+
+        // Obtén nuevamente la lista actualizada de municipios
+        $municipios = DB::table('tb_municipio')
+            ->orderBy('muni_nomb')
+            ->get();
+
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
+
+        return view('comunas.index', ['comunas' => $comunas, 'municipios' => $municipios]);
     }
+    
 
     /**
      * Display the specified resource.
@@ -62,7 +90,17 @@ class ComunaController extends Controller
      */
     public function edit($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $municipios = DB::table('tb_municipio')->orderBy('muni_nomb')->get();
+        $departamentos = DB::table('tb_departamento')->orderBy('depa_nomb')->get();
+        $paises = DB::table('tb_pais')->orderBy('pais_nomb')->get(); // Obtener los países
+
+    return view('comunas.edit', [
+        'comuna' => $comuna,
+        'municipios' => $municipios,
+        'departamentos' => $departamentos,
+        'paises' => $paises, // Pasar los países a la vista
+    ]);
     }
 
     /**
@@ -74,7 +112,17 @@ class ComunaController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $comuna = Comuna::find($id);
+        
+        $comuna->comu_nomb = $request->name;
+        $comuna->muni_codi = $request->code;
+        $comuna->save();
+    
+        $comunas = DB::table('tb_comuna')
+            ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+            ->select('tb_comuna.*', 'tb_municipio.muni_nomb')
+            ->get();
+        return view('comunas.index', ['comunas' => $comunas]); 
     }
 
     /**
@@ -85,6 +133,15 @@ class ComunaController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $comuna = Comuna::find($id);
+        $comuna -> delete();
+
+
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio' , 'tb_comuna.muni_codi', '= ','tb_municipio.muni_  codi') 
+        ->select('tb_comuna.*', "tb_municipio.muni_nombe")
+        ->get();
+
+        return  view ('comunas.index' ,['comunas'=> $comunas]);
     }
 }
